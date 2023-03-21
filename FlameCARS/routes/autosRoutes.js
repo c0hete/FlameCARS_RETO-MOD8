cc0e7474ad60748c5aa635b268783e9d3fb9355b
José Rafael Alvarado Mazzei <jose.alvarado.mazzei@gmail.com>
2023-03-21 19:39:34 -0300

    Proyecto antes de implementarJWT

---
+++ FlameCARS/routes/autosRoutes.js
@@ -0,0 +1,217 @@
+const express = require('express');
+const router = express.Router();
+const { pool } = require("../config/dbConfig");
+const multer  = require('multer');
+const initializePassport = require('../config/passportConfig');
+const passport = require('passport');
+initializePassport(passport);
+
+
+
+// Middleware de autenticación
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect('/users/login');
+  }
+}
+
+// Ruta que requiere autenticación
+router.get('/users/dashboard', ensureAuthenticated, async (req, res) => {
+  const userId = req.user.id;
+  const query = 'SELECT * FROM autos WHERE id = $1';
+  const values = [userId];
+  const result = await pool.query(query, values);
+  const auto = result.rows[0];
+
+  try {
+    const autoResult = await pool.query(
+      'SELECT id, foto, marca, modelo, anio FROM autos WHERE id_propietario = $1 ORDER BY id ASC LIMIT 5',
+      [userId]
+    );
+
+    const autos = autoResult.rows;
+
+    console.log(autos); // Agrega este console.log para verificar que los datos se hayan cargado correctamente
+
+    res.render('dashboard', { autos }); // Pasando la variable autos
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving user profile');
+  }
+});
+
+router.get("/users/:userId/image/:imageId", async (req, res) => {
+  const { userId, imageId } = req.params;
+  const imagePath = path.join(__dirname, '..', 'public', 'img', imageId);
+  res.sendFile(imagePath);
+});
+
+async function getAutos() {
+  const query = 'SELECT * FROM autos';
+  const client = await pool.connect();
+  try {
+    const result = await client.query(query);
+    return result.rows;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.release();
+  }
+}
+
+// Configuración de Multer para manejar la carga de archivos
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/img/') // Cambiar 'public/uploads/' por 'public/images/'
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  });
+  const upload = multer({ storage: storage });
+
+  async function getAutos() {
+    const query = `
+      SELECT autos.*, users.name AS propietario_nombre
+      FROM autos
+      INNER JOIN users ON autos.id_propietario = users.id;
+    `;
+    const { rows } = await pool.query(query);
+    return rows;
+  }
+
+router.post('/update', async (req, res) => {
+  const { id, marca, modelo, anio } = req.body;
+  try {
+    const query = 'UPDATE autos SET marca = $1, modelo = $2, anio = $3 WHERE id = $4';
+    const values = [marca, modelo, anio, id];
+    await pool.query(query, values);
+    res.redirect('/users/dashboard');
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+
+// Obtener todos los autos
+module.exports = async (req, res) => {
+  try {
+    const autos = await getAutos();
+    res.render('autos', { autos });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+};
+
+// Obtener todos los autos
+router.get('/', async (req, res) => {
+  try {
+    const autos = await getAutos();
+    res.render('autos', { user: req.user, autos: autos })
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+
+// Obtener un auto por ID
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const query = 'SELECT * FROM autos WHERE id = $1';
+    const values = [id];
+    const result = await pool.query(query, values);
+    const auto = result.rows[0];
+    if (!auto) {
+      return res.sendStatus(404);
+    }
+    res.render('autos', { auto });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+// Crear un auto
+router.post('/', upload.single('foto'), async (req, res) => {
+  if (!req.isAuthenticated()) {
+    res.redirect('/users/login');
+    return;
+  }
+  const foto = req.file.filename;
+  const propietario_id = req.user.id; // Obtener el id del propietario desde la sesión
+  const { marca, modelo, anio, estado, precio } = req.body;
+  try {
+    const query = 'INSERT INTO autos (marca, modelo, anio, id_propietario, foto) VALUES ($1, $2, $3, $4, $5) RETURNING id';
+    const values = [marca, modelo, anio, propietario_id, foto];
+    const result = await pool.query(query, values);
+    const autoId = result.rows[0].id;
+    res.redirect(`/autos`);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+
+router.get('/users/dashboard', async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const userResult = await pool.query(
+      'SELECT email, specialization FROM users WHERE id = $1',
+      [userId]
+    );
+
+    const autoResult = await pool.query(
+      'SELECT id, foto FROM autos WHERE id_propietario = $1 ORDER BY id ASC LIMIT 3',
+      [userId]
+    );
+
+    const user = userResult.rows[0];
+    const autos = autoResult.rows;
+
+    res.render('dashboard', { user, autos });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving user profile');
+  }
+});
+
+
+
+// Actualizar un auto
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { marca, modelo, anio } = req.body;
+  try {
+    const query = 'UPDATE autos SET marca = $1, modelo = $2, anio = $3 WHERE id = $4';
+    const values = [marca, modelo, anio, id];
+    await pool.query(query, values);
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+// Eliminar un auto
+router.post('/delete', async (req, res) => {
+  const { id } = req.body;
+  try {
+    const query = 'DELETE FROM autos WHERE id = $1';
+    const values = [id];
+    await pool.query(query, values);
+    res.redirect('/users/dashboard');
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+module.exports = router;
---
+++ FlameCARS/routes/usersRoutes.js
@@ -0,0 +1,201 @@
+const { Router } = require("express");
+const { pool } = require("../config/dbConfig");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const router = Router();
+const passport = require("passport");
+const multer = require("multer");
+
+//MULTER
+// Definir el almacenamiento para los archivos cargados
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/img");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null, 
+      file.fieldname +
+        "-" +
+        Date.now() +
+        "." +
+        file.originalname.split(".").pop()
+    ); // crear un nombre único para el archivo
+  },
+});
+// Configurar multer
+const upload = multer({ storage: storage });
+
+// Middleware para verificar si el usuario está autenticado
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/users/login');
+}
+
+router.get("/", (req, res) => {
+  res.render('index', { user: req.user });
+});
+
+router.get('/', function(req, res) {
+  res.render('index', { user: req.user, message: 'You have logged out successfully' });
+});
+
+router.get("/users/login", (req, res) => res.render("login", { user: req.user }));
+
+
+router.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/users/dashboard',
+  failureRedirect: '/users/login',
+  failureFlash: true
+}), (req, res) => {
+  req.session.userId = req.user.id;
+  res.redirect('/users/dashboard');
+});
+
+router.get('/users/register', (req, res) => {
+  res.render('register', { user: req.user });
+
+});
+
+router.get("/users/dashboard", isLoggedIn, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const query = `SELECT * FROM users WHERE id = ${userId}`;
+    const result = await pool.query(query);
+    const user = result.rows[0];
+    const autos = await pool.query('SELECT * FROM autos WHERE id_propietario = $1', [userId]);
+    res.render('dashboard', { user: user.name, userId: user.id, autos: autos.rows });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+router.get("/users/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.render("index", { user: req.user, message: "You have logged out successfully" });
+  });
+});
+
+router.get('/users/:id/image', async (req, res) => {
+  const id = req.params.id;
+  const query = `SELECT photo FROM users WHERE id = ${id}`;
+  try {
+    const result = await pool.query(query);
+    const user = result.rows[0];
+    const imagePath = path.join(__dirname, '..', 'public', 'img', user.photo);
+    res.sendFile(imagePath);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+router.get('/autos/:id/image', async (req, res) => {
+  const id = req.params.id;
+  const query = `SELECT foto FROM autos WHERE id = ${id}`;
+  try {
+    const result = await pool.query(query);
+    const auto = result.rows[0];
+    const imagePath = path.join(__dirname, '..', 'public', 'img', auto.foto);
+    res.sendFile(imagePath);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+router.post("/users/register", upload.single("photo"), async (req, res) => {
+  // manejar la imagen cargada
+  const photo = req.file;
+
+  let { name, email, password, password2, years, specialization } = req.body;
+
+  console.log({
+    name,
+    email,
+    password,
+    password2,
+    years,
+    specialization,
+    photo,
+  });
+
+  let errors = [];
+
+  if (
+    !name ||
+    !email ||
+    !password ||
+    !password2 ||
+    !years ||
+    !specialization ||
+    !photo
+  ) {
+    errors.push({ message: "Please enter all fields" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ message: "Passwords should be at least 6 characters" });
+  }
+
+  if (password != password2) {
+    errors.push({ message: "Passwords do not match" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", { errors, user: req.user });
+
+  } else {
+    //form validation has passed
+
+    let hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
+    pool.query(
+      `SELECT * FROM users
+            WHERE email = $1`,
+      [email],
+      (err, results) => {
+        if (err) {
+          throw err;
+        }
+
+        console.log(results.rows);
+        if (results.rows.length > 0) {
+          errors.push({ message: "Email already registered" });
+          res.render("register", { errors });
+        } else {
+          pool.query(
+            `INSERT INTO users (name, email, password, years, specialization, photo)
+                  VALUES ($1, $2, $3, $4, $5, $6)
+                  RETURNING id, password`,
+            [
+              name,
+              email,
+              hashedPassword,
+              years,
+              specialization,
+              photo.filename,
+            ],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              console.log(results.rows);
+              req.flash("success_msg", "You are now registered. Please log in");
+              res.redirect("/users/login");
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
+module.exports = router;
